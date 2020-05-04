2db3da64c26238a5cbc4f369edf8b9809574a551
Erik Hollensbe <github@hollensbe.org>
2020-05-03 23:04:02 -0700

    whacky benchmarks which are more about stress testing easily, than anything
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- ldnsd_test.go
+++ ldnsd_test.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
 	"net"
 	"os"
+	"runtime"
+	"strconv"
 	"testing"
 	"time"
 
@@ -12,6 +16,16 @@ import (
 	"code.hollensbe.org/erikh/ldnsd/service"
 	"github.com/miekg/dns"
 )
+
+func init() {
+	seed := os.Getenv("TEST_SEED")
+	s, err := strconv.ParseInt(seed, 10, 64)
+	if err != nil {
+		s = time.Now().Unix()
+		fmt.Println("Seed: ", s)
+	}
+	rand.Seed(s)
+}
 
 const (
 	defaultCAFile    = "/etc/ldnsd/rootCA.pem"
@@ -65,6 +79,152 @@ func BenchmarkDNSSingleDomain(b *testing.B) {
 		b.RunParallel(func(pb *testing.PB) {
 			for pb.Next() {
 				m, err := msgClient("test.internal.")
+				if err != nil {
+					b.Log(err)
+					continue
+				}
+
+				aRecord := m.Answer[0].(*dns.A).A
+				if !aRecord.Equal(ip) {
+					b.Fatalf("IP %q does not match registered IP %q", aRecord, ip)
+				}
+			}
+		})
+	})
+}
+
+func randString(count, min int) string {
+	s := []rune{}
+	for i := 0; i < rand.Intn(count-min)+min; i++ {
+		s = append(s, rune('a'+rand.Intn(26)))
+	}
+
+	return string(s)
+}
+
+func BenchmarkRecordInsert(b *testing.B) {
+	srv, err := startService()
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer srv.Shutdown()
+	defer os.Remove("test.db")
+
+	client, err := proto.NewClient(config.DefaultGRPCListen, defaultCAFile, defaultCertFile, defaultKeyFile)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	hostChan := make(chan string, runtime.NumCPU()*2)
+	hosts := map[string]struct{}{}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go func(ctx context.Context) {
+		for {
+		retry:
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+
+			host := randString(30, 3)
+			if _, ok := hosts[host]; ok {
+				goto retry
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case hostChan <- host:
+				hosts[host] = struct{}{}
+			}
+		}
+	}(ctx)
+
+	b.Run("record insert", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				if _, err := client.SetA(context.Background(), &proto.Record{Host: <-hostChan, Address: "1.2.3.4"}); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	})
+
+}
+
+func BenchmarkRecordInsertThenQuery(b *testing.B) {
+	srv, err := startService()
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer srv.Shutdown()
+	defer os.Remove("test.db")
+
+	client, err := proto.NewClient(config.DefaultGRPCListen, defaultCAFile, defaultCertFile, defaultKeyFile)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	ip := net.ParseIP("1.2.3.4")
+
+	// no buffer here otherwise we won't be able to resolve the on-buffer items that will get pushed to the map
+	hostChan := make(chan string)
+	hosts := map[string]struct{}{}
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func(ctx context.Context) {
+		for {
+		retry:
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
+
+			host := randString(30, 3)
+			if _, ok := hosts[host]; ok {
+				goto retry
+			}
+
+			select {
+			case hostChan <- host:
+				hosts[host] = struct{}{}
+			case <-ctx.Done():
+				return
+			}
+		}
+	}(ctx)
+
+	b.Run("record insert", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				if _, err := client.SetA(context.Background(), &proto.Record{Host: <-hostChan, Address: "1.2.3.4"}); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	})
+
+	cancel()
+	hostChan = make(chan string, runtime.NumCPU()*2)
+
+	go func() {
+		for {
+			for key := range hosts {
+				hostChan <- key
+			}
+		}
+	}()
+
+	b.Run("query", func(b *testing.B) {
+		b.RunParallel(func(pb *testing.PB) {
+			for pb.Next() {
+				m, err := msgClient(fmt.Sprintf("%s.internal.", <-hostChan))
 				if err != nil {
 					b.Log(err)
 					continue
