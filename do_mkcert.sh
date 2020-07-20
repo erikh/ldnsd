db4e869e28014632855bee61891ae6b630908e6d
Erik Hollensbe <github@hollensbe.org>
2020-07-19 17:01:37 -0700

    agh
    Signed-off-by: Erik Hollensbe <github@hollensbe.org>

--- do_mkcert.sh
+++ do_mkcert.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+mkdir -p /etc/ldnsd
+
 mkcert -install && \
   mkcert -ecdsa -cert-file /etc/ldnsd/server.pem -key-file /etc/ldnsd/server.key localhost 127.0.0.1 && \
   mkcert -ecdsa -client -cert-file /etc/ldnsd/client.pem -key-file /etc/ldnsd/client.key localhost 127.0.0.1
